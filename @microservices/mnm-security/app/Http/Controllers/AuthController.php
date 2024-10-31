b77806b4aa85aaa8e47dfd91d0f202c66658bc8d
JhosueLeo <150406643+JhosueLeo@users.noreply.github.com>
2024-10-30 23:00:13 -0500

    Register Culminado

--- @microservices/mnm-security/app/Http/Controllers/AuthController.php
+++ @microservices/mnm-security/app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
 class AuthController extends Controller
@@ -39,7 +40,7 @@ class AuthController extends Controller
                 'refresh_token_expires_at' => $expiracion_refresh,
             ]);
             return response()->json([
-                "usuario"=>
+                "usuario" =>
                 [
                     'user_id' => $usuario->user_id,
                     'name' => $usuario->name,
@@ -51,18 +52,54 @@ class AuthController extends Controller
                     'verified_state' => $usuario->verified_state,
                     'verified_at' => $usuario->verified_at,
                 ],
-                "token"=>
+                "token" =>
                 [
                     'access_token' => $acceso_token,
                     'refresh_token' => $refresh_token,
                     'access_token_expires_at' => $expiracion_acceso,
                     'refresh_token_expires_at' => $expiracion_refresh,
                 ]
-            ],200);
+            ], 200);
         } catch (TooManyRequestsHttpException $e) {
             return response()->json(['message' => 'Demasiados intentos fallidos. Inténtalo de nuevo en 1 minutos'], 400);
-        } catch (\Throwable $th) {
-            return response()->json(['message' => 'Data invalida'], 400);
+        } catch (\Exception $e) {
+            return response()->json(["error" => "Algo salió mal", "message" => $e->getMessage()], 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $request->validate([
+                'name' => 'required|string',
+                'paternal_surname' => 'required|string',
+                'maternal_surname' => 'required|string',
+                'data_of_birth' => 'required|date',
+                'email' => 'required|email',
+                'password' => 'required|string|min:4',
+            ]);
+            $usuario_existente=User::where('status','A')->where('email', $request->email)->first();
+            if($usuario_existente){
+                return response()->json(['resp' => 'El correo ya se encuentra registrado'], 400);
+            }
+            User::create([
+                'user_id' => User::max('user_id') + 1,
+                'name' => $request->name,
+                'paternal_surname' => $request->paternal_surname,
+                'maternal_surname' => $request->maternal_surname,
+                'data_of_birth' => $request->data_of_birth,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'user_type' => 2,
+                'verified_state' => 'V',
+                'verified_at' => now(),
+            ]);
+            DB::commit();
+            return response()->json(['resp'=>'Registrado Correctamente'], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(["error" => "Algo salió mal", "message" => $e->getMessage()], 500);
         }
     }
 }
--- @microservices/mnm-security/routes/api.php
+++ @microservices/mnm-security/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'Auth'], function () {
     Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
+    Route::post('/register', [AuthController::class, 'register']);
 });
